c95397b56faa0bd46f0a52e5c4bdfa0e2c770326
Ulhas Daga <ulhasdaga@gmail.com>
2024-11-07 12:06:34 +0530

    3rd problem

--- problem3.java
+++ problem3.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class problem3 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        sc.close();
+        long largest = 1;
+        System.out.println("reached here");
+        for(long i = 2; n != 1; i++){
+            /*if(i % 2 != 0){
+            if(isPrime(i) && (n % i == 0))
+                largest = i;
+            }*/
+            while(n % i == 0){
+                n = n / i;
+                largest = i;
+            }
+            //System.out.println("Largest is: " + largest);
+        }
+        System.out.println(largest);
+    }
+    /*public static boolean isPrime(long a){
+        System.out.println("Checking for: "+ a);
+        if(a % 2 == 0)
+        return false;
+        for(long i = 3; i <= a / 2; i++){
+            if(a % i == 0)
+                return false;
+        }
+        return true;
+    }*/
 }
